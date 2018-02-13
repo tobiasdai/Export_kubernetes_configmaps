32f01f567ac117122f5e5d10baf30d9caef33a3e
Tobiiii <tobiasdsh@outlook.com>
2018-02-13 10:24:05 +0800

    final (maybe there's still bugs :D)

---
+++ export.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+# 自动import脚本
+. ./auto_export_configmaps.sh
+. ./auto_export_deployments.sh
+. ./auto_export_secrets.sh
+. ./auto_export_services.sh
---
+++ import.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+. ./auto_import_configmaps.sh
+. ./auto_import_deployments.sh
+. ./auto_import_secrets.sh
+. ./auto_import_services.sh
+
